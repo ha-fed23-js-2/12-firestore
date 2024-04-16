adcd8f883426c79e2b319b611b1cabf318ca6fd9
David Andersson <lejonmanen@gmail.com>
2024-04-16 14:18:24 +0200

    Initial commit

---
+++ src/data/fire.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBfYhcBAlOvnEVnQPrqZkHUpUC9sqn5JIc",
+	authDomain: "ha-fed-23.firebaseapp.com",
+	projectId: "ha-fed-23",
+	storageBucket: "ha-fed-23.appspot.com",
+	messagingSenderId: "640038849374",
+	appId: "1:640038849374:web:d554e90d13ae8b53de1dcf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
